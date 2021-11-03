a233868066cd4a8834a2e631cf20d3d98ccecc3a
Mager <andrew.mager@gmail.com>
2021-11-03 12:15:13 -0500

    Update disclaimer

--- components/Disclaimer/index.tsx
+++ components/Disclaimer/index.tsx
@@ -26,6 +26,16 @@ const Disclaimer = () => {
           lineHeight: '1.5rem',
         })}
       >
+        There&rsquo;s currently an issue with OpenSea&rsquo;s API where all
+        floor prices are 0. Looking into it...
+      </p>
+      <p
+        className={css({
+          paddingLeft: '2.5rem',
+          paddingRight: '2.5rem',
+          lineHeight: '1.5rem',
+        })}
+      >
         Currently, we only support fetching 50 collections & 250 NFTs. Polygon
         NFTs are not supported yet. NFTs without a contract are not supported
         yet. Trait floor prices coming soon. Please report any bugs or feature
